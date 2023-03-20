063a5ef2bce6b1eeb0d84c9c113caf650ce7be75
noyvirto <106173842+krkenCoding@users.noreply.github.com>
2023-03-20 21:58:43 +0000

    Update outputformat.py
    Cleaner output overall achieved

--- outputformat.py
+++ outputformat.py
@@ -2,11 +2,12 @@ import re, datetime, os
 
 # Opening the output file (the result of the arp scan)
 f = open("output.txt", "r")
-content = f.readlines()
+outputContent = f.readlines()
 f.close()
 
 existingDevices = []
 updatedDevices = []
+lastLine = outputContent[-1].replace("\n", "")
 
 # checking to see if existingDevices.txt exists. This means that multiple scans will be ran.
 if os.path.isfile("./existingDevices.txt"):
@@ -20,8 +21,8 @@ if os.path.isfile("./existingDevices.txt"):
         line = line.replace("\n", "")
         existingDevices.append(line)
 
-    # open the second output
-    f = open("output2.txt", "r")
+    # open the output file
+    f = open("output.txt", "r")
     content = f.readlines()
     f.close()
     for line in content:
@@ -37,20 +38,29 @@ if os.path.isfile("./existingDevices.txt"):
             else:
                 updatedDevices.append(line)
 
-    print()
-    print("Devices that have since connected:")
+    #print(".")
+    newDevices = []
     f = open("existingDevices.txt", "a")
     for device in updatedDevices:
         if device not in existingDevices:
-            print(device)
             existingDevices.append(device)
+            newDevices.append(device)
+    if len(newDevices) > 0:
+        print("Devices that have since connected:")
+        for device in newDevices:
+            print(device)
+        #print(".")
 
-    print()
-    print("Devices that have since disconnected:")
+    legacyDevices = []
     for device in existingDevices:
         if device not in updatedDevices:
             existingDevices.remove(device)
+            legacyDevices.append(device)
+    if len(legacyDevices) > 0:
+        print("Devices that have since disconnected:")
+        for device in legacyDevices:
             print(device)
+
     f.close()
     f = open("existingDevices.txt", "w")
     for line in existingDevices:
@@ -58,23 +68,32 @@ if os.path.isfile("./existingDevices.txt"):
         f.write("\n")
     quit()
 
-lastLine = content[-1].replace("\n", "")
+
 
 # Checking if multiple scans will be ran, if so a file needs to be created
-if lastLine == "*":
+elif lastLine == "*":
+    # creates the file existingDevices.txt
     f = open("existingDevices.txt", "w")
-    for line in content:
+    # go through all the lines of output
+    for line in outputContent:
+        # remove the \n that's in all lines
         line = line.replace("\n", "")
+        # try splitting the lines into elements, we want the first element
+        # some lines have no content hence the try
         try:
             firstElement = line.split()[0]
         except IndexError:
             continue
+        # if the first element matches the ipv4 format then...
         match = re.match(r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}", firstElement)
         if bool(match) == True:
+            # be sure the device isn't a dup which is common
             if "(DUP:" in line.split():
                 continue
             else:
+                # if not a dupe then write the line into existingDevices.txt
                 f.write(line)
+                # newline
                 f.write("\n")
     f.close()
 
@@ -82,7 +101,7 @@ devicesFound = False
 deviceCount = 0
 
 # Going through every line of the file
-for line in content:
+for line in outputContent:
     line = line.replace("\n", "")
     try:
         firstElement = line.split()[0]
